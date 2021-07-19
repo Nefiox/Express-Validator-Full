13023feae397f2ca4d1a62689a6b5003b8196247
Juan Jiménez <nefioxdev@gmail.com>
2021-07-18 20:59:51 -0500

    Integrating Multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,10 @@ const controller = {
 	register: (req, res) => {
 		return res.render('userRegisterForm');
 	},
+	processRegister: (req, res) => {
+		console.log(req.body);
+		res.send(req.body)
+	},
 	login: (req, res) => {
 		return res.render('userLoginForm');
 	},
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const expressValidator = require('express-validator');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '../public/images/avatars');
+    },
+    filename: (req, file, cb) => {
+        let fileName = `avatar-${Date.now()}_img${path.extname(file.filename)} `
+        cb(null, fileName);
+    }
+});
+
+const uploadFile = multer({ storage });
 
 const usersController = require('../controllers/userController');
 
 // Formulario de registro
 router.get('/register', usersController.register);
+
+// Procesar formulario de registro
+router.post('/register', uploadFile.single('avatar'), usersController.processRegister);
 
 // Formulario de login
 router.get('/login', usersController.login);
