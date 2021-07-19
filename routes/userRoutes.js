c273f5e6038705b8805ed81b4b20d94dfb964f2a
Juan Jiménez <nefioxdev@gmail.com>
2021-07-19 13:16:33 -0500

    Showing errors in register form

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const expressValidator = require('express-validator');
+const { body } = require('express-validator');
 const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '../public/images/avatars');
+        cb(null, './public/images/avatars');
     },
     filename: (req, file, cb) => {
-        let fileName = `avatar-${Date.now()}_img${path.extname(file.filename)} `
+        let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
         cb(null, fileName);
     }
 });
@@ -18,11 +18,18 @@ const uploadFile = multer({ storage });
 
 const usersController = require('../controllers/userController');
 
+const validations = [
+    body('fullName').notEmpty().withMessage('Tienes que completar el nombre'),
+    body('email').notEmpty().withMessage('Tienes que completar el email'),
+    body('password').notEmpty().withMessage('Tienes que completar la contraseña'),
+    body('country').notEmpty().withMessage('Tienes que completar el país')
+];
+
 // Formulario de registro
 router.get('/register', usersController.register);
 
 // Procesar formulario de registro
-router.post('/register', uploadFile.single('avatar'), usersController.processRegister);
+router.post('/register', uploadFile.single('avatar'), validations, usersController.processRegister);
 
 // Formulario de login
 router.get('/login', usersController.login);
